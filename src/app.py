8c4260c182b3bda3495b4b0d8d9353de2f2f7ca4
Baku-D-Shaggy <diego.mouchet@gmail.com>
2025-07-21 07:49:59 +0000

    vF

--- src/app.py
+++ src/app.py
@@ -1,40 +1,48 @@
 from utils import db_connect
 engine = db_connect()
+
 from flask import Flask, render_template, request
 import pickle
-import numpy as np
+import os
 
 # your code here
 
-app = Flask(__name__)
 
-# Load model
-with open('iris_model.pkl', 'rb') as f:
+
+# Get the current directory of this script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Create the Flask app with custom template folder
+app = Flask(__name__, template_folder=os.path.join(current_dir, '../templates'))
+
+# Load the model
+model_path = os.path.join(current_dir, 'iris_model.pkl')
+with open(model_path, 'rb') as f:
     model = pickle.load(f)
 
-# Species mapping
-species = {
-    0: "Iris Setosa",
-    1: "Iris Versicolor",
-    2: "Iris Virginica"
+# Define a dictionary for class names
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica"
 }
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    prediction = None
     if request.method == 'POST':
-        # Get form values
-        features = [
-            float(request.form['sepal_length']),
-            float(request.form['sepal_width']),
-            float(request.form['petal_length']),
-            float(request.form['petal_width'])
-        ]
-        # Predict
-        result = model.predict([features])[0]
-        prediction = species[result]
+        # Get the values from the form
+        val1 = float(request.form['sepal_length'])
+        val2 = float(request.form['sepal_width'])
+        val3 = float(request.form['petal_length'])
+        val4 = float(request.form['petal_width'])
         
-    return render_template('index.html', prediction=prediction)
+        data = [[val1, val2, val3, val4]]
+        prediction = str(model.predict(data)[0])
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    
+    return render_template('index.html', prediction=pred_class)
 
 if __name__ == '__main__':
     app.run(debug=True)
